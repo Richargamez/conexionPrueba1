5bed8419b9c639a3d1367290dd511d51ca0b7165
Richargamez <richar.gamez@hotmail.com>
2017-02-22 20:54:06 -0700

    nuevo form

---
+++ Pruebagithub/Pruebagithub/Prueba2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pruebagithub
+{
+    public partial class Prueba2 : Form
+    {
+        public Prueba2()
+        {
+            InitializeComponent();
+        }
+    }
+}
